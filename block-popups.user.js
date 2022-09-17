8136f5b70fee612bec1d2ae6e79c8615ee7ec40d
Igor Dimitrijević <igor.dvlpr@gmail.com>
2022-09-17 02:19:51 +0200

    Update block-popups.user.js

--- block-popups.user.js
+++ block-popups.user.js
@@ -2,8 +2,10 @@
 // @name           Block all popup APIs
 // @namespace      https://github.com/igorskyflyer
 // @description    Blocks all APIs used for popups, WILL BREAK certain sites
+// @grant          unsafeWindow
 // @include        *
+// @runat          document-start
 // ==/UserScript==
 
-window.open = function() {}
-location.replace = function() {}
+unsafeWindow.open = function() {}
+unsafeWindow.location.replace = function() {}
